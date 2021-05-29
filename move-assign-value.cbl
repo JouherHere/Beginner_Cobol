e8143a527ecd0475b21f518b7c71c8c1e9889f88
Jouher Ali <84992201+JouherHere@users.noreply.github.com>
2021-05-29 16:56:26 +0530

    Create move-assign-value.cbl

---
+++ move-assign-value.cbl
@@ -0,0 +1,20 @@
+//* A program to illustrate value assign and movement in variable //*
+//*      
+      
+      IDENTIFICATION DIVISION.                        
+       PROGRAM-ID. ASMOVMOD.                               
+       ENVIRONMENT DIVISION.                           
+       DATA DIVISION.                                  
+       WORKING-STORAGE SECTION.                        
+       77 EMPID1 PIC 9(5) VALUE 5564.                   
+       77 EMPID2 PIC 9(5) VALUE 6756.                            
+       77 EMPNAME PIC X(10) VALUE 'JOUHER'.             
+       PROCEDURE DIVISION. 
+           DISPLAY 'EMPID1=' EMPID1.                              
+           DISPLAY 'EMPID2=' EMPID2.
+           MOVE EMPID1 TO EMPID2.       
+           DISPLAY '**DATA AFTER MOVEMENT OF VALUE**'
+           DISPLAY 'EMPID1=' EMPID1.                              
+           DISPLAY 'EMPID2=' EMPID2.                             
+           DISPLAY EMPNAME.                             
+           STOP RUN.  
