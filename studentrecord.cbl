9f2dad1d982b6c3c1456bd8865fff8da36b09c29
Jouher Ali <84992201+JouherHere@users.noreply.github.com>
2021-05-29 16:19:01 +0530

    Create studentrecord.cbl
    A simple program to create and print student record.

---
+++ studentrecord.cbl
@@ -0,0 +1,28 @@
+//*A simple program to create and print student record*//
+// https://www.tutorialspoint.com/compile_cobol_online.php //
+//*You can use the above mentioned compiler to run the code//*      
+
+IDENTIFICATION DIVISION.                     
+        PROGRAM-ID. STUDREC.                            
+        ENVIRONMENT DIVISION.                        
+        DATA DIVISION.                               
+        WORKING-STORAGE SECTION.                     
+        01 RECMOD.                                       
+            02 ROLLNO PIC 9(3).                      
+            02 FILLER PIC X.                         
+            02 STUDNAME PIC A(8).                     
+            02 FILLER PIC X.                         
+            02 MARK1 PIC 9(3).                       
+            02 FILLER PIC X.                         
+            02 MARK2 PIC 9(3).                       
+            02 FILLER PIC X.                         
+            02 MARK3 PIC 9(3).       
+       PROCEDURE DIVISION.  
+           MOVE 9        TO ROLLNO.
+           MOVE 'JOUHER' TO STUDNAME.  
+           MOVE 55       TO MARK1.
+           MOVE 65       TO MARK2.
+           MOVE 87       TO MARK3.
+      
+           DISPLAY RECMOD               
+           STOP RUN.
