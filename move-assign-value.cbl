3954b1af7a0a8e2eace7cabd98e096fb6b0596d7
Jouher Ali <84992201+JouherHere@users.noreply.github.com>
2021-12-31 00:00:05 +0530

    Update move-assign-value.cbl

--- move-assign-value.cbl
+++ move-assign-value.cbl
@@ -1,15 +1,18 @@
-//* A program to illustrate value assign and movement in variable //*
-//*      
+ 
+      ********************************************
+      * COBOL PROGRAM TO ILLUSTRATE VALUE ASSIGN *
+      * AND MOVEMENT IN VARIABLE                 *
+      ********************************************
       
       IDENTIFICATION DIVISION.                        
-       PROGRAM-ID. ASMOVMOD.                               
-       ENVIRONMENT DIVISION.                           
-       DATA DIVISION.                                  
-       WORKING-STORAGE SECTION.                        
-       77 EMPID1 PIC 9(5) VALUE 5564.                   
-       77 EMPID2 PIC 9(5) VALUE 6756.                            
-       77 EMPNAME PIC X(10) VALUE 'JOUHER'.             
-       PROCEDURE DIVISION. 
+      PROGRAM-ID. ASMOVMOD.                               
+      ENVIRONMENT DIVISION.                           
+      DATA DIVISION.                                  
+      WORKING-STORAGE SECTION.                        
+      77 EMPID1 PIC 9(5) VALUE 5564.                   
+      77 EMPID2 PIC 9(5) VALUE 6756.                            
+      77 EMPNAME PIC X(10) VALUE 'JOUHER'.             
+      PROCEDURE DIVISION. 
            DISPLAY 'EMPID1=' EMPID1.                              
            DISPLAY 'EMPID2=' EMPID2.
            MOVE EMPID1 TO EMPID2.       
